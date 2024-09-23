61b0457e45a0b1762dd87719a6ca5da57e2894f8
Mattias Thalén <bitter-polders0x@icloud.com>
2024-09-23 13:47:11 +0200

    No need to coalesce valid_to after staging

--- models/silver/links/link__order__store.sql
+++ models/silver/links/link__order__store.sql
@@ -8,7 +8,7 @@ select
 ,   store_hk
 ,   source
 ,   min(valid_from) as valid_from
-,   max(coalesce(valid_to, '9999-12-31 23:59:59'::timestamp)) as valid_to
+,   max(valid_to) as valid_to
 
 from
     silver.stg__jaffle_shop__orders
