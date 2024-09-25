fa9e621bd8c6e928005d05793db07ec47bc2b1ee
Mattias Thalén <bitter-polders0x@icloud.com>
2024-09-25 16:40:14 +0200

    Fix typo in dim__stores

--- models/gold/dimensions/dim__stores.sql
+++ models/gold/dimensions/dim__stores.sql
@@ -11,7 +11,7 @@ SELECT
   store_pit_hk, /* Point in time hash key of the store */
   id AS store_id, /* Natueral key of the store */
   name AS store_name, /* Name of the store */
-  opened_at AS asstore_opened_at, /* Timestamp of when the store opened */
+  opened_at AS store_opened_at, /* Timestamp of when the store opened */
   tax_rate AS store_tax_rate, /* Tax rate for the store */
   filename AS store_filename, /* Filename of the import */
   source AS store_record_source, /* Source of the store record */
