d1fba0a196c7fd4def5070e3e8a4b0e41c6fdf8c
Mattias Thalén <bitter-polders0x@icloud.com>
2024-10-03 10:45:11 +0200

    Update hub macro to follow the same pattern as the link macro

--- macros/data_vault__load_hub.py
+++ macros/data_vault__load_hub.py
@@ -15,7 +15,7 @@ def data_vault__load_hub(
     if not isinstance(sources, exp.Tuple):
         sources = exp.Tuple(expressions=[sources])
 
-    cte__union__body: str = " UNION ALL ".join(
+    cte__union: str = " UNION ALL ".join(
         f"""
             SELECT
                 {index} as source_index,
@@ -37,9 +37,12 @@ def data_vault__load_hub(
         for index, source in enumerate(sources)
     )
 
-    cte__union: str = f"cte__union AS ({cte__union__body})"
-    cte__distinct: str = f"cte__distinct AS (SELECT DISTINCT ON ({business_key}) * FROM cte__union ORDER BY {business_key}, source_index)"
-    cte: str = f"WITH {cte__union}, {cte__distinct}"
+    cte__distinct: str = f"""
+        SELECT DISTINCT ON ({business_key})
+            *
+        FROM cte__union
+        ORDER BY {business_key}, source_index
+    """
 
     start = evaluator.locals.get("start_ts")
     end = evaluator.locals.get("end_ts")
@@ -49,6 +52,11 @@ def data_vault__load_hub(
     if start and end:
         where = f"WHERE {updated_at} BETWEEN '{start}' AND '{end}'"
 
-    sql: str = f"{cte} SELECT * FROM cte__distinct {where};"
+    sql: str = f"""
+        WITH
+            cte__union AS ({cte__union})
+        ,   cte__distinct AS ({cte__distinct})
+        SELECT * FROM cte__distinct {where};
+    """
 
     return sql
