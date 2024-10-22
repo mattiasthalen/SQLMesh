db91a1c725736c47af38febfbe90174923d295ac
Mattias Thalén <bitter-polders0x@icloud.com>
2024-10-22 12:37:51 +0200

    Refactor api model for weather data

--- models/bronze/raw__meteostat__point__daily.py
+++ models/bronze/raw__meteostat__point__daily.py
@@ -8,27 +8,29 @@ from datetime import datetime
 from sqlmesh import ExecutionContext, model
 from sqlmesh.core.model.kind import ModelKindName
 
+columns={
+      "latitude": "text",
+      "longitude": "text",
+      "date": "text",
+      "tavg": "text",
+      "tmin": "text",
+      "tmax": "text",
+      "prcp": "text",
+      "snow": "text",
+      "wdir": "text",
+      "wspd": "text",
+      "wpgt": "text",
+      "pres": "text",
+      "tsun": "text"
+  }
+
 @model(
     cron="@daily",
     kind=dict(
         name=ModelKindName.INCREMENTAL_BY_TIME_RANGE,
         time_column="date"
     ),
-    columns={
-        "latitude": "text",
-        "longitude": "text",
-        "date": "text",
-	    "tavg": "text",
-	    "tmin": "text",
-	    "tmax": "text",
-	    "prcp": "text",
-	    "snow": "text",
-	    "wdir": "text",
-	    "wspd": "text",
-	    "wpgt": "text",
-	    "pres": "text",
-	    "tsun": "text"
-    }
+    columns=columns
 )
 
 def execute(
@@ -38,9 +40,6 @@ def execute(
     execution_time: datetime,
     **kwargs: t.Any,
 ) -> t.Generator[pd.DataFrame, None, None]:
-    
-    # no_data = yield from ()
-    # return no_data
     
     raw__seed__cities = context.table("bronze.raw__seed__cities")
     raw__seed__cities__df = context.fetchdf(f"SELECT * FROM {raw__seed__cities}")
@@ -52,7 +51,7 @@ def execute(
         "x-rapidapi-host": "meteostat.p.rapidapi.com"
     }
 
-    all_data = []
+    final_df = pd.DataFrame({column: pd.Series(dtype='str') for column, data_type in columns.items()})
 
     for _, row in raw__seed__cities__df.iterrows():
         querystring = {
@@ -62,9 +61,14 @@ def execute(
             "end": end.strftime('%Y-%m-%d')
         }
 
-        for attempt in range(5):
+        response = None
+        success = False
+        n_attempts = 3
+        
+        for attempt in range(n_attempts):
             response = requests.get(url, headers=headers, params=querystring)
             if response.status_code == 200:
+                success = True
                 break
             elif response.status_code == 429:
                 sleep_duration = 1
@@ -73,13 +77,15 @@ def execute(
             else:
                 print(f"Error fetching data for {row['city']}: {response.status_code}")
                 break
-
-        if response.status_code != 200:
+        
+        if not success:
+            print(f"Failed to fetch data for {row['city']} after {n_attempts} attempts")
             continue
-
-        data = response.json()
+        
+        data = response.json() if response else {}
+        
         if 'data' not in data:
-            print(f"No data found for {row['city']}")
+            print(f"No 'data' key found in response for {row['city']}. Response: {data}")
             continue
 
         df = pd.DataFrame(data['data'])
@@ -89,8 +95,13 @@ def execute(
 
         print(f"{row['city']}: {len(df)}")
 
-        all_data.append(df.astype(str))
+        final_df = pd.concat([final_df, df.astype(str)], ignore_index=True)
 
-    final_df = pd.concat(all_data, ignore_index=True)
+    n_rows = len(final_df)
+    
+    print(f"Yielding DataFrame with {n_rows} rows.")
 
-    return final_df
+    if n_rows > 0:
+        yield final_df
+    else:
+        yield from ()
